55f86ea237a38333ce7179e25f1daebf9adec7da
MohammedAqeel18 <aqeelworkmail@gmail.com>
2025-04-15 00:57:52 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/contact', contactRoute);
+const path = require("path");
+
+// Serve static files from React frontend
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // <-- this line is critical
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
